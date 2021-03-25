0da540fa583a6662172b3d6a37c02cc4ad434979
87Alex <39731874+87Alex@users.noreply.github.com>
2021-03-25 13:12:00 +0300

    Update ShowFilterDialogController.vb

--- VB/E1554.Module/ShowFilterDialogController.vb
+++ VB/E1554.Module/ShowFilterDialogController.vb
@@ -1,4 +1,3 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports DevExpress.ExpressApp
 Imports DevExpress.ExpressApp.SystemModule
@@ -7,93 +6,125 @@ Imports DevExpress.ExpressApp.Editors
 Imports DevExpress.Data.Filtering
 
 Namespace E1554.Module
-	Public Class ShowFilterDialogController
-		Inherits WindowController
-		Public Sub New()
-			TargetWindowType = WindowType.Main
-		End Sub
+    Public Class ShowFilterDialogController
+        Inherits WindowController
 
-		Private showNavigationItemController As ShowNavigationItemController
-		Private oldListView As ListView
+        Public Sub New()
+            TargetWindowType = WindowType.Main
+        End Sub
 
-		Protected Overrides Sub OnActivated()
-			MyBase.OnActivated()
-			showNavigationItemController = Frame.GetController(Of ShowNavigationItemController)()
-			If showNavigationItemController IsNot Nothing Then
-				AddHandler showNavigationItemController.ShowNavigationItemAction.Execute, AddressOf ShowNavigationItemAction_Execute
-				AddHandler showNavigationItemController.CustomUpdateSelectedItem, AddressOf showNavigationItemController_CustomUpdateSelectedItem
-			End If
-		End Sub
+        Private showNavigationItemController As ShowNavigationItemController
+        Private oldListView As ListView
+        Private selectedChoiceActionItem As ChoiceActionItem
+        Private showFromController As Boolean = False
 
-		Private Sub ShowNavigationItemAction_Execute(ByVal sender As Object, ByVal e As SingleChoiceActionExecuteEventArgs)
-			If TypeOf e.ShowViewParameters.CreatedView Is ListView Then
-				oldListView = CType(e.ShowViewParameters.CreatedView, ListView)
-				Dim nonPersistentObjectSpace As NonPersistentObjectSpace = CType(Application.CreateObjectSpace(GetType(ViewFilterContainer)), NonPersistentObjectSpace)
-				Dim persistentObjectSpace As IObjectSpace = Application.CreateObjectSpace(GetType(ViewFilterObject))
-				nonPersistentObjectSpace.AdditionalObjectSpaces.Add(persistentObjectSpace)
-				Dim newViewFilterContainer As ViewFilterContainer = nonPersistentObjectSpace.CreateObject(Of ViewFilterContainer)()
-				newViewFilterContainer.ObjectType = oldListView.ObjectTypeInfo.Type
-				newViewFilterContainer.Filter = GetFilterObject(persistentObjectSpace, (CType(oldListView.Model, IModelListViewAdditionalCriteria)).AdditionalCriteria, newViewFilterContainer.ObjectType)
-				Dim filterDetailView As DetailView = Application.CreateDetailView(nonPersistentObjectSpace, newViewFilterContainer)
-				filterDetailView.Caption = String.Format("Filter for the {0} ListView", oldListView.Caption)
-				filterDetailView.ViewEditMode = ViewEditMode.Edit
-				e.ShowViewParameters.CreatedView = filterDetailView
-				e.ShowViewParameters.TargetWindow = TargetWindow.NewModalWindow
-				Dim dialogCotnroller As DialogController = Application.CreateController(Of DialogController)()
-				AddHandler dialogCotnroller.Accepting, AddressOf dialogCotnroller_Accepting
-				AddHandler dialogCotnroller.ViewClosed, AddressOf dialogCotnroller_ViewClosed
-				e.ShowViewParameters.Controllers.Add(dialogCotnroller)
-			End If
-		End Sub
+        Protected Overrides Sub OnActivated()
+            MyBase.OnActivated()
+            showNavigationItemController = Frame.GetController(Of ShowNavigationItemController)()
 
-		Private Sub dialogCotnroller_Accepting(ByVal sender As Object, ByVal e As DialogControllerAcceptingEventArgs)
-			Dim currentViewFilterContainer As ViewFilterContainer = CType(e.AcceptActionArgs.CurrentObject, ViewFilterContainer)
-			Dim targetView As ListView = GetTargetView()
-			CType(targetView.Model, IModelListViewAdditionalCriteria).AdditionalCriteria = currentViewFilterContainer.Criteria
-			targetView.CollectionSource.Criteria("ByViewFilterObject") = CriteriaEditorHelper.GetCriteriaOperator(currentViewFilterContainer.Criteria, currentViewFilterContainer.ObjectType, targetView.ObjectSpace)
-			Dim parameters As New ShowViewParameters(targetView)
-			parameters.TargetWindow = TargetWindow.Current
-			parameters.Context = TemplateContext.View
-			Dim source As New ShowViewSource(Frame, showNavigationItemController.ShowNavigationItemAction)
-			Application.ShowViewStrategy.ShowView(parameters, source)
-			oldListView = Nothing
-		End Sub
+            If showNavigationItemController IsNot Nothing Then
+                Me.showNavigationItemController.CustomShowNavigationItem += AddressOf ShowNavigationItemController_CustomShowNavigationItem
+            End If
+        End Sub
 
-		Protected Overridable Function GetTargetView() As ListView
-			Return oldListView
-		End Function
+        Private Sub ShowNavigationItemAction_Executed(ByVal sender As Object, ByVal e As ActionBaseEventArgs)
+            Me.showNavigationItemController.ShowNavigationItemAction.Executed -= AddressOf ShowNavigationItemAction_Executed
 
-		Private Sub dialogCotnroller_ViewClosed(ByVal sender As Object, ByVal e As EventArgs)
-			oldListView = Nothing
-		End Sub
+            If showFromController Then
+                e.ShowViewParameters.CreatedView = oldListView
+            End If
+        End Sub
 
-		Private Function GetFilterObject(ByVal objectSpace As IObjectSpace, ByVal listViewCriteria As String, ByVal objectType As Type) As ViewFilterObject
-			Dim criteria As CriteriaOperator = CriteriaOperator.Parse("Criteria = ? and ObjectType = ?", listViewCriteria, objectType)
-			Dim filterObject As ViewFilterObject = objectSpace.FindObject(Of ViewFilterObject)(criteria)
-			If filterObject Is Nothing Then
-				filterObject = objectSpace.CreateObject(Of ViewFilterObject)()
-				filterObject.ObjectType = objectType
-				filterObject.Criteria = listViewCriteria
-				filterObject.FilterName = "Default"
-			End If
-			Return filterObject
-		End Function
+        Private Sub ShowNavigationItemController_CustomShowNavigationItem(ByVal sender As Object, ByVal e As CustomShowNavigationItemEventArgs)
+            If showFromController Then
+                Return
+            End If
 
-		Private Sub showNavigationItemController_CustomUpdateSelectedItem(ByVal sender As Object, ByVal e As CustomUpdateSelectedItemEventArgs)
-			If oldListView IsNot Nothing Then
-				e.ProposedSelectedItem = showNavigationItemController.FindNavigationItemByViewShortcut(oldListView.CreateShortcut())
-				e.Handled = True
-			End If
-		End Sub
+            Dim shortcut As ViewShortcut = TryCast(e.ActionArguments.SelectedChoiceActionItem.Data, ViewShortcut)
 
-		Protected Overrides Sub OnDeactivated()
-			MyBase.OnDeactivated()
-			If showNavigationItemController IsNot Nothing Then
-				RemoveHandler showNavigationItemController.ShowNavigationItemAction.Execute, AddressOf ShowNavigationItemAction_Execute
-				RemoveHandler showNavigationItemController.CustomUpdateSelectedItem, AddressOf showNavigationItemController_CustomUpdateSelectedItem
-				showNavigationItemController = Nothing
-			End If
-			oldListView = Nothing
-		End Sub
-	End Class
+            If shortcut IsNot Nothing Then
+                oldListView = TryCast(Application.ProcessShortcut(shortcut), ListView)
+            End If
+
+            If oldListView IsNot Nothing Then
+                e.Handled = True
+                selectedChoiceActionItem = e.ActionArguments.SelectedChoiceActionItem
+                Dim nonPersistentObjectSpace As NonPersistentObjectSpace = CType(Application.CreateObjectSpace(GetType(ViewFilterContainer)), NonPersistentObjectSpace)
+                Dim persistentObjectSpace As IObjectSpace = Application.CreateObjectSpace(GetType(ViewFilterObject))
+                nonPersistentObjectSpace.AdditionalObjectSpaces.Add(persistentObjectSpace)
+                Dim newViewFilterContainer As ViewFilterContainer = nonPersistentObjectSpace.CreateObject(Of ViewFilterContainer)()
+                newViewFilterContainer.ObjectType = oldListView.ObjectTypeInfo.Type
+                newViewFilterContainer.Filter = GetFilterObject(persistentObjectSpace, CType(oldListView.Model, IModelListViewAdditionalCriteria).AdditionalCriteria, newViewFilterContainer.ObjectType)
+                Dim filterDetailView As DetailView = Application.CreateDetailView(nonPersistentObjectSpace, newViewFilterContainer)
+                filterDetailView.Caption = String.Format("Filter for the {0} ListView", oldListView.Caption)
+                filterDetailView.ViewEditMode = ViewEditMode.Edit
+                e.ActionArguments.ShowViewParameters.CreatedView = filterDetailView
+                e.ActionArguments.ShowViewParameters.TargetWindow = TargetWindow.NewModalWindow
+                Dim dialogCotnroller As DialogController = Application.CreateController(Of DialogController)()
+                                ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node) in D:\GitWorkspace\CodeConverter\CodeConverter\VB\NodesVisitor.cs:line 978
+'''    at Microsoft.CodeAnalysis.CSharp.Syntax.AssignmentExpressionSyntax.Accept[TResult](CSharpSyntaxVisitor`1 visitor)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping) in D:\GitWorkspace\CodeConverter\CodeConverter\VB\CommentConvertingVisitorWrapper.cs:line 26
+''' 
+''' Input:
+'''                 dialogCotnroller.Accepting += new System.EventHandler<DialogControllerAcceptingEventArgs>(this.dialogCotnroller_Accepting)
+''' 
+                dialogCotnroller.ViewClosed += AddressOf dialogCotnroller_ViewClosed
+                e.ActionArguments.ShowViewParameters.Controllers.Add(dialogCotnroller)
+            End If
+        End Sub
+
+        Private Sub dialogCotnroller_Accepting(ByVal sender As Object, ByVal e As DialogControllerAcceptingEventArgs)
+            Dim currentViewFilterContainer As ViewFilterContainer = CType(e.AcceptActionArgs.CurrentObject, ViewFilterContainer)
+            Dim targetView As ListView = GetTargetView()
+            CType(targetView.Model, IModelListViewAdditionalCriteria).AdditionalCriteria = currentViewFilterContainer.Criteria
+            targetView.CollectionSource.Criteria("ByViewFilterObject") = CriteriaEditorHelper.GetCriteriaOperator(currentViewFilterContainer.Criteria, currentViewFilterContainer.ObjectType, targetView.ObjectSpace)
+            Me.showNavigationItemController.ShowNavigationItemAction.Executed += AddressOf ShowNavigationItemAction_Executed
+
+            Try
+                showFromController = True
+                showNavigationItemController.ShowNavigationItemAction.DoExecute(selectedChoiceActionItem)
+            Finally
+                showFromController = False
+                oldListView = Nothing
+                selectedChoiceActionItem = Nothing
+            End Try
+        End Sub
+
+        Protected Overridable Function GetTargetView() As ListView
+            Return oldListView
+        End Function
+
+        Private Sub dialogCotnroller_ViewClosed(ByVal sender As Object, ByVal e As EventArgs)
+            oldListView = Nothing
+            selectedChoiceActionItem = Nothing
+        End Sub
+
+        Private Function GetFilterObject(ByVal objectSpace As IObjectSpace, ByVal listViewCriteria As String, ByVal objectType As Type) As ViewFilterObject
+            Dim criteria As CriteriaOperator = CriteriaOperator.Parse("Criteria = ? and ObjectType = ?", listViewCriteria, objectType)
+            Dim filterObject As ViewFilterObject = objectSpace.FindObject(Of ViewFilterObject)(criteria)
+
+            If filterObject Is Nothing Then
+                filterObject = objectSpace.CreateObject(Of ViewFilterObject)()
+                filterObject.ObjectType = objectType
+                filterObject.Criteria = listViewCriteria
+                filterObject.FilterName = "Default"
+            End If
+
+            Return filterObject
+        End Function
+
+        Protected Overrides Sub OnDeactivated()
+            MyBase.OnDeactivated()
+
+            If showNavigationItemController IsNot Nothing Then
+                Me.showNavigationItemController.CustomShowNavigationItem -= AddressOf ShowNavigationItemController_CustomShowNavigationItem
+                showNavigationItemController = Nothing
+            End If
+
+            oldListView = Nothing
+            selectedChoiceActionItem = Nothing
+        End Sub
+    End Class
 End Namespace
